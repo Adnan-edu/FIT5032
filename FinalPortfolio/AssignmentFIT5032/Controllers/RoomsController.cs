5046a88c5c8d5449d7261734c675a0eb1dad1abe
Adnan <md.ashikadnan@gmail.com>
2019-09-25 14:40:16 +1000

    Assignment Portfolio with individual authentication

---
+++ FinalPortfolio/AssignmentFIT5032/Controllers/RoomsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AssignmentFIT5032.Models;
+
+namespace AssignmentFIT5032.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private AssignmentFit5032ModelContainer db = new AssignmentFit5032ModelContainer();
+
+        // GET: Rooms
+        public ActionResult Index()
+        {
+            var rooms = db.Rooms.Include(r => r.Hotel);
+            return View(rooms.ToList());
+        }
+
+        // GET: Rooms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // GET: Rooms/Create
+        public ActionResult Create()
+        {
+            ViewBag.HotelId = new SelectList(db.Hotels, "Id", "Name");
+            return View();
+        }
+
+        // POST: Rooms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,RoomName,Description,HotelId")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rooms.Add(room);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.HotelId = new SelectList(db.Hotels, "Id", "Name", room.HotelId);
+            return View(room);
+        }
+
+        // GET: Rooms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.HotelId = new SelectList(db.Hotels, "Id", "Name", room.HotelId);
+            return View(room);
+        }
+
+        // POST: Rooms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,RoomName,Description,HotelId")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(room).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.HotelId = new SelectList(db.Hotels, "Id", "Name", room.HotelId);
+            return View(room);
+        }
+
+        // GET: Rooms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Room room = db.Rooms.Find(id);
+            db.Rooms.Remove(room);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
