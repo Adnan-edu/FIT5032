fa39606a4a587fb5c4246444150e31cf96fa46ec
Adnan <md.ashikadnan@gmail.com>
2019-10-12 12:31:21 +1100

    Added EmailWithAttachment for Assessment3

---
+++ Assessment 3/EmailWithAttachment/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using EmailWithAttachment.Models;
+using EmailWithAttachment.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmailWithAttachment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Send_Email(SendEmailViewModel model)
+        {
+            try
+            {
+                string path = Server.MapPath("~/App_Data/uploads");
+                string fileName = Path.GetFileName(model.AttachedFile.FileName);
+                string fullPath = Path.Combine(path, fileName);
+                model.AttachedFile.SaveAs(fullPath);
+
+                EmailSender es = new EmailSender();
+                es.Send(model, fullPath);
+
+                ViewBag.Result = "Email has been send.";
+
+                ModelState.Clear();
+
+                return View(new SendEmailViewModel());
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ Assessment 3/EmailWithAttachment/Utils/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using EmailWithAttachment.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace EmailWithAttachment.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(SendEmailViewModel model, String fullPath)
+        {
+            string fileName = Path.GetFileName(model.AttachedFile.FileName);
+
+            var bytes = System.IO.File.ReadAllBytes(fullPath);
+            var file = Convert.ToBase64String(bytes);
+            var client = new SendGridClient(API_KEY);
+
+            var from = new EmailAddress("noreply@localhost.com", "FIT5032 Example Email User");
+            //var to = new EmailAddress(toEmail, "");
+
+            var to_emails = new List<EmailAddress>
+            {
+                new EmailAddress(model.ToEmail, "Example User1")
+            };
+
+
+            var msg = new SendGridMessage();
+            msg.SetFrom("noreply@localhost.com", "FIT5032 Assessment3");
+            msg.PlainTextContent = model.Contents;
+            msg.Subject = model.Subject;
+            msg.AddAttachment(model.AttachedFile.FileName, file);
+            msg.AddTos(to_emails);
+            var response = client.SendEmailAsync(msg);
+        }
+    }
+}
